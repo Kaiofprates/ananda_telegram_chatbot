d83d40d11451ef5b2b9501334577a4f358af78fb
kaiofprates <kaioprates@hotmail.com>
2020-03-09 00:27:28 -0300

    add quotes

--- main.js
+++ main.js
@@ -7,6 +7,35 @@ const axios = require('axios');
 
 let session_id  = false; 
 
+
+const quotes = [
+"Leia ao menos 5 páginas de qualquer coisa por dia.",
+"Elogie alguém",
+"Pare ao menos 5 minutos para tentar não pensar em nada",
+"Coverse com alguém sobre qualquer assunto que vocé ainda não domine e a pessoa sim",
+"Procure evitar estar sempre em lugares onde você será a atenção",
+"Observe mais",
+"Deixe seu ego de lado e aceite a opinião dos outros",
+"Pare de cobrar dos outros ser ouvido",
+"Lembre-se de quem se lembra de vc",
+"Fale um pouco mais baixo",
+"Deixe algumas coisas se perderem mesmo, procure entender a real necessidade do que mais consome seu tempo",
+"Compartilhe algo de bom",
+"Nunca peça algo em troca de algum favor",
+"Não espere muito ou nada de ninguem",
+"Não espere nada de ninguem",
+"Não espere ninguem",
+"Nunca deixe de ter um sonho",
+"Escreva||leia ao menos uma página de qualquer coisa por dia",
+"Respeite seu sono",
+"Deixe ir",
+"Não espere nada de ninguem",
+"O apego pelo passado gera a depressão. O apego pelo furturo gera a anciedade. O viver do agora constroi a felicidade",
+"Nunca deixe de sonhar"
+]
+
+
+
 // ---------------- busca por vagas --------------------
 bot.onText(/\/vagas (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
@@ -40,6 +69,10 @@ if(msg.text == "#mandanude"){
     bot.sendMessage(chatId, 'Oiiii sumido, tudo bem?');
 }else if(msg.text  == 'Tudo e vc?'){
     bot.sendMessage(chatId, 'Melhor agora ;)');
+}else if(msg.text.toLowerCase() == 'me ajude' || msg.text.toLowerCase() == "ajuda" || msg.text.toLowerCase() == "bom dia"){
+    var item = quotes[Math.floor(Math.random() * quotes.length)]
+    bot.sendMessage(chatId,item )
+
 }else if(msg.text == "config"){
 
   axios.get('http://localhost:3000/api/session').then(function (response) {
