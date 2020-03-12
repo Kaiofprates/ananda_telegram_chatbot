ff9dbe27fa25a8609dd1548c3224449f2780f15c
kaiofprates <kaioprates@hotmail.com>
2020-03-12 00:52:15 -0300

    improving session control

--- main.js
+++ main.js
@@ -2,37 +2,8 @@ const {getJob} = require('./src/middlewares/linkedin/jobs');
 const {mandaNude} = require('./src/middlewares/nude/sendNude');
 const {bot}  = require('./src/servers/server');
 const {getWhois}  = require('./src/middlewares/whois/whois');
-const axios = require('axios');
 const { sendMessage, getSessionId } = require('./src/servers/assistent');
 const { getCache, setCache } = require('./src/servers/redis');
-
-let session_id  = false; 
-
-const quotes = [
-"Leia ao menos 5 páginas de qualquer coisa por dia.",
-"Elogie alguém",
-"Pare ao menos 5 minutos para tentar não pensar em nada",
-"Coverse com alguém sobre qualquer assunto que vocé ainda não domine e a pessoa sim",
-"Procure evitar estar sempre em lugares onde você será a atenção",
-"Observe mais",
-"Deixe seu ego de lado e aceite a opinião dos outros",
-"Pare de cobrar dos outros ser ouvido",
-"Lembre-se de quem se lembra de vc",
-"Fale um pouco mais baixo",
-"Deixe algumas coisas se perderem mesmo, procure entender a real necessidade do que mais consome seu tempo",
-"Compartilhe algo de bom",
-"Nunca peça algo em troca de algum favor",
-"Não espere muito ou nada de ninguem",
-"Não espere nada de ninguem",
-"Não espere ninguem",
-"Nunca deixe de ter um sonho",
-"Escreva||leia ao menos uma página de qualquer coisa por dia",
-"Respeite seu sono",
-"Deixe ir",
-"Não espere nada de ninguem",
-"O apego pelo passado gera a depressão. O apego pelo furturo gera a anciedade. O viver do agora constroi a felicidade",
-"Nunca deixe de sonhar"
-]
 
 
 // ---------------- busca por vagas --------------------
@@ -64,11 +35,7 @@ if(msg.text == "#mandanude"){
     bot.sendMessage(chatId, 'Oiiii sumido, tudo bem?');
 }else if(msg.text  == 'Tudo e vc?'){
     bot.sendMessage(chatId, 'Melhor agora ;)');
-}else if(msg.text.toLowerCase() == 'me ajude' || msg.text.toLowerCase() == "ajuda" || msg.text.toLowerCase() == "bom dia"){
-    var item = quotes[Math.floor(Math.random() * quotes.length)]
-    bot.sendMessage(chatId,item )
-
-}else if(msg.text.toLowerCase() == "config"){
+}else if(msg.text.toLowerCase() == "ola watson"){
   
   try{  
     
@@ -81,16 +48,23 @@ if(msg.text == "#mandanude"){
     console.log(err)
   }
 
-}else if(msg.text && session_id){
-
+}else if(msg.text){
+   
    try{
      let id  = await getCache(chatId);
      if(id){
       let response  = await sendMessage(msg.text,id);
       bot.sendMessage(chatId,response);
+     }else{
+      let id = await getSessionId();
+      console.log(id);
+      setCache(chatId,id.result.session_id);
+      bot.sendMessage(chatId,'Oiii, não consegui te entender! Diga novamente!')
      }
+
    }catch(err){
-     console.log(err)
+   
    }
 }});
+
 
